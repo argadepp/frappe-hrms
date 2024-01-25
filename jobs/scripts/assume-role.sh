445e36e0b912e5f874e4321f8b467a5b8819701d
pratik3686 <pratik3686@gmail.com>
2024-01-25 17:32:20 +0530

    jenkins file added

---
+++ jobs/scripts/assume-role.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+roleArn="arn:aws:iam::734522607489:role/terraform-infra-create-role"
+
+aws sts assume-role --role-arn $roleArn --role-session-name deploy-frappe --output json --duration 3600 > out.json
+
+
+accessKey=$(jq -r '.Credentials.AccessKeyId' out.json)
+secretKey=$(jq -r '.Credentials.SecretAccessKey' out.json)
+sessionToken=$(jq -r '.Credentials.SessionToken' out.json)
+
+
+aws configure set aws_access_key_id $accessKey
+aws configure set aws_secret_access_key $secretKey
+aws configure set aws_session_token $sessionToken

d13ddf4947aecc2f08dd7ab1d5e765b14df85e90
Pratik Pralhad Argade <90461882+argadepp@users.noreply.github.com>
2024-01-31 11:18:07 +0530

    Update main.tf

--- provision/main.tf
+++ provision/main.tf
@@ -9,6 +9,11 @@ resource "aws_instance" "hrms_inst" {
     private_key = file("~/.ssh/gaction1.pem")
     user = "ubuntu"
   }
+
+  root_block_device {
+    volume_size = 20
+    volume_type = gp2
+  }
   provisioner "remote-exec" {
     inline = [
         "git clone https://github.com/argadepp/frappe-hrms.git && cd frappe-hrms",

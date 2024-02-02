a65544cdfa5331d5dc46ac1910078eb6dc52c1d4
pratik3686 <pratik3686@gmail.com>
2024-02-02 12:27:00 +0530

    script changes

--- scripts/install_hrms.sh
+++ scripts/install_hrms.sh
@@ -8,8 +8,8 @@ sudo apt install docker.io docker-compose -y
 sudo usermod -aG docker ubuntu
 sudo chmod 777 /var/run/docker.sock
 sleep 20s
-# git clone https://github.com/argadepp/frappe-hrms.git
-# cd frappe-hrms
+git clone https://argadepp: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@github.com/argadepp/frappe-hrms.git
+cd frappe-hrms
 
 docker-compose up -d
 

0b519a94818223d824c01206db70b0f1ab56ffc9
pratik3686 <pratik3686@gmail.com>
2024-02-02 16:19:08 +0530

    enable https

--- scripts/make_https.sh
+++ scripts/make_https.sh
@@ -9,3 +9,5 @@ NEW_VALUE=$1
 sed -i "s/^${KEY_TO_REPLACE}=.*/${KEY_TO_REPLACE}=${NEW_VALUE}/" .env
 
 docker-compose -f caddy-compose.yaml up -d
+
+echo "Enabled successfully https on site "https://$NEW_VALUE" , please access it."

55f460a74eda2f05dbab415ca21ca335db027de7
pratik3686 <pratik3686@gmail.com>
2024-02-05 11:56:59 +0530

    Few changes in scripts

--- scripts/install_hrms.sh
+++ scripts/install_hrms.sh
@@ -17,7 +17,7 @@ docker exec $app"_frappe_1" bash -c "cd /home/frappe && bench init --skip-redis-
 docker cp common_site_config.json $app"_frappe_1":/home/frappe/frappe-bench/sites/
 docker exec $app"_frappe_1" bash -c "cd /home/frappe/frappe-bench && bench get-app erpnext && bench get-app hrms"
 docker exec $app"_frappe_1" bash -c "cd /home/frappe/frappe-bench && bench new-site $domain --force --no-mariadb-socket --admin-password=admin --db-host=mariadb --db-root-password=123 --install-app erpnext --set-default"
-docker exec $app"_frappe_1" bash -c "cd /home/frappe/frappe-bench && bench --site hrms install-app hrms"
+docker exec $app"_frappe_1" bash -c "cd /home/frappe/frappe-bench && bench --site $domain install-app hrms"
 docker exec $app"_frappe_1" bash -c "cd /home/frappe/frappe-bench && bench start &"
 
 # Define variables
